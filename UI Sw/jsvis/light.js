1c6d1f2fdae65a6fa158c4517fb562fc8fa9d7d6
Joshua Hampp <joshua.hampp@gmx.de>
2012-11-17 11:21:54 +0100

    ui: beautify

--- UI Sw/jsvis/light.js
+++ UI Sw/jsvis/light.js
@@ -1,7 +1,7 @@
 gl_vis_temp['light'] = {
 	init: function($this) {
-		$this.append('<img id="on" style="display:inline;position:absolute" src="'+$this.data("vis").img.on+'" />')
-		$this.append('<img id="off" style="display:inline;position:absolute" src="'+$this.data("vis").img.off+'" />')
+		$this.append('<img id="on" style="display:inline;" src="'+$this.data("vis").img.on+'" />')
+		$this.append('<img id="off" style="display:inline;" src="'+$this.data("vis").img.off+'" />')
 		
 		$this.find('img#on').hide();
 		$this.find('img#off').hide();
