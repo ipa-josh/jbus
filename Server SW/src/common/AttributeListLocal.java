8a79afc65eb6b4c49509fdb24b0ec274e4f04dde
Joshua Hampp <joshua.hampp@gmx.de>
2012-12-18 20:18:47 +0100

    server: update local attribute

--- Server SW/src/common/AttributeListLocal.java
+++ Server SW/src/common/AttributeListLocal.java
@@ -1,6 +1,8 @@
 package common;
 
 import HWDriver.AVRNETIO.AvrNetIoBoard;
+import HWDriver.JBUS.JBusInterface;
+import HWDriver.SUNSET.Sunset;
 import common.attributes.Attr_Boolean;
 import common.attributes.Attr_Integer;
 import common.attributes.Attr_String;
@@ -9,7 +11,7 @@ public class AttributeListLocal implements AttributeList {
 
 	@Override
 	public Class[] getList() {
-		return new Class [] {HAObject.class, Attr_Boolean.class, Attr_Integer.class, Attr_String.class, AvrNetIoBoard.class};
+		return new Class [] {HAObject.class, Attr_Boolean.class, Attr_Integer.class, Attr_String.class, AvrNetIoBoard.class, Sunset.class, JBusInterface.class};
 	}
 
 }
