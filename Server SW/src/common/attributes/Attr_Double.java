87c8e12b2e8f1bed3aa34a4a98f1cd8cbba5d3d1
Joshua Hampp <joshua.hampp@gmx.de>
2012-12-18 20:23:13 +0100

    server: added double attr

---
+++ Server SW/src/common/attributes/Attr_Double.java
@@ -0,0 +1,60 @@
+package common.attributes;
+
+import java.util.Calendar;
+
+import org.jdom2.Element;
+
+import rights.Group;
+import rights.User;
+import common.Attribute;
+
+public class Attr_Double extends Attribute {
+	double val_;
+
+	public Attr_Double(User usr, Group grp, Attribute parent) {
+		super(usr, grp, parent);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean _set(User usr, Object v) {
+		if(v==null)
+			return false;
+
+		double t;
+		if(Double.class.equals(v.getClass())) {
+			t=(Double)v;
+		}
+		else if(Float.class.equals(v.getClass())) {
+			t=(Float)v;
+		}
+		else if(Integer.class.equals(v.getClass())) {
+			t=(Integer)v;
+		}
+		else if(Long.class.equals(v.getClass())) {
+			t=(Long)v;
+		}
+		else if(String.class.equals(v.getClass())) {
+			t=Long.parseLong((String)v);
+		}
+		else
+			return false;
+		if(t==val_)
+			return false;
+		val_ = t;
+
+		ts_change_=Calendar.getInstance().getTimeInMillis();
+		return true;
+	}
+
+	@Override
+	protected Object _get(User usr) {
+		return new Double(val_);
+	}
+	
+	public boolean _readXML(Element el) {
+		val_ = Double.parseDouble(el.getValue());
+		return true;
+	}
+
+}
