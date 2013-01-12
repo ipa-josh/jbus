3f1d5c571f85380f8bee19b94fb99308b141164a
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2013-01-12 15:26:18 +0100

    server: optimized notifier

--- Server SW/src/common/Notifier.java
+++ Server SW/src/common/Notifier.java
@@ -5,23 +5,44 @@ import java.util.Vector;
 public class Notifier {
 
 	private Vector<Callback> notifier_ = new Vector<Callback>();
+
+	public int notify(Attribute attr) {
+		return notify(attr, -1);
+	}
 	
-	public boolean notify(Attribute attr) {
-		for(int i=0; i<notifier_.size(); i++) {
+	public int notify(Attribute attr, int _size) {
+		int size = notifier_.size();
+		if(_size>=0)
+			size = _size;
+		for(int i=0; i<size; i++) {
 			if(notifier_.get(i).required())
 				if(!notifier_.get(i).onAttributeChanged(attr))
-					return false;
+					return i;
 		}
+
+		if(_size<0)
+			for(int i=0; i<size; i++) {
+				if(!notifier_.get(i).required())
+					notifier_.get(i).onAttributeChanged(attr);
+			}
 		
-		for(int i=0; i<notifier_.size(); i++) {
-			if(!notifier_.get(i).required())
-				notifier_.get(i).onAttributeChanged(attr);
-		}
-		
-		return true;
+		return -1;
 	}
 	
 	public void addCallback(Callback c) {
 		notifier_.add(c);
+	}
+	
+	public static interface RemovalCondition {
+		public boolean canRemove(Callback c);
+	}
+	
+	public void remove(String clname, RemovalCondition rc) {
+		for(int i=0; i<notifier_.size(); i++) {
+			if(notifier_.get(i)!=null && notifier_.get(i).getClass().getSimpleName().equals(clname) && rc.canRemove(notifier_.get(i)) ) {
+				notifier_.remove(i);
+				--i;
+			}
+		}
 	}
 }
