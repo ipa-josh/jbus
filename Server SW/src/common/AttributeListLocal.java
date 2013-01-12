4326252d5f0c997b5b77f5b3b3a02cb397fdf29d
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2013-01-12 15:24:59 +0100

    server: made new features public

--- Server SW/src/common/AttributeListLocal.java
+++ Server SW/src/common/AttributeListLocal.java
@@ -1,8 +1,14 @@
 package common;
 
 import HWDriver.AVRNETIO.AvrNetIoBoard;
+import HWDriver.COMMAND.Command;
+import HWDriver.COMMAND.Editor;
+import HWDriver.CONVERTER.Temperature;
 import HWDriver.JBUS.JBusInterface;
+import HWDriver.LOGIC.AND;
+import HWDriver.LOGIC.OR;
 import HWDriver.SUNSET.Sunset;
+import HWDriver.SUNSET.Time;
 import common.attributes.Attr_Boolean;
 import common.attributes.Attr_Integer;
 import common.attributes.Attr_String;
@@ -11,7 +17,7 @@ public class AttributeListLocal implements AttributeList {
 
 	@Override
 	public Class[] getList() {
-		return new Class [] {HAObject.class, Attr_Boolean.class, Attr_Integer.class, Attr_String.class, AvrNetIoBoard.class, Sunset.class, JBusInterface.class};
+		return new Class [] {HAObject.class, Attr_Boolean.class, Attr_Integer.class, Attr_String.class, AvrNetIoBoard.class, Sunset.class, Time.class, AND.class, OR.class, JBusInterface.class, Temperature.class, Command.class, Editor.class};
 	}
 
 }
